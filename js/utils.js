a0fb9f23fb39f51a95999afbcbd9585fd30f269e
vania <6058960@qq.com>
2018-09-18 15:29:53 +0800

    [commit]
    home page and common methods

--- js/utils.js
+++ js/utils.js
@@ -6,3 +6,18 @@ Utils.prototype.appendModule = function(selector) {
 	var module = document.querySelector(selector).import.querySelector('script');
 	document.querySelector('head').appendChild(module);
 }
+
+/**
+ * @params [String] linkSelector 导入模板link id 
+ * @params [String] tplId 模板id
+ * @params [String] innerSelector 插入html dom
+ * @params [Object] data 数据
+ * @params [function] callback 回调方法
+ */
+Utils.prototype.innerTplHtml = function(tplId, innerSelector ,data, callback) {
+	var html = template(tplId, data);
+	if (html) 
+		document.querySelector(innerSelector).innerHTML = html;
+	if (typeof callback === 'function') 
+		callback();
+}
