f13dc8e4f652f789334b0cd458190ccc2b1bf1de
wangwenbin0035 <2994707742@qq.com>
2018-12-28 14:09:49 +0800

    pad 需求新增盘点中得内容，其余内容部分bug 修改
    update

--- js/utils.js
+++ js/utils.js
@@ -16,7 +16,6 @@ Utils.prototype.appendModule = function(selector) {
  * @params [function] callback 回调方法
  */
 Utils.prototype.innerTplHtml = function(tplId, innerSelector ,data, insertType ,callback) {
-	console.log(insertType,'insertType')
 	var html = template(tplId, data);
 	if (html) {
 		if(!insertType){
